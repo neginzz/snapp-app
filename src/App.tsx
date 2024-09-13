9855bd9749b82f9ca5485e9786b28a7772a2005e
neginz <negin.z566@gmail.com>
2024-09-14 00:11:43 +0330

    [feat] set up  app  and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+// src/App.tsx
 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchLocPage from "./pages/SearchLocPage";
+import DirectionPage from "./pages/DirectionPage";
+import LoadingPage from "./pages/LoadingPage";
+import DriverPage from "./pages/DriverPage";
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<SearchLocPage />} />
+        <Route path="/directions" element={<DirectionPage />} />
+        <Route path="/loading" element={<LoadingPage />} />
+        <Route path="/driver" element={<DriverPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
